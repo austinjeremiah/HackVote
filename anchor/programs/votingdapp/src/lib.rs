d596b3cee5f14d33c9809c955164517d091004f3
Austin Jeremiah J <139321386+austinjeremiah@users.noreply.github.com>
2024-12-15 23:41:01 +0530

    Updated function for lib.rs

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::result_large_err)]
+#![allow(unexpected_cfgs)]
 
 use anchor_lang::prelude::*;
 
@@ -8,4 +9,32 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod votingdapp {
     use super::*;
 
+    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE, 
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>, 
+    pub system_program: Program<'info, System>,
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {  
+    pub poll_id: u64,
+    #[max_len(300)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
 }
