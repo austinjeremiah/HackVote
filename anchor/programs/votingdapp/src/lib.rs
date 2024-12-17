6c05a24f7991d5048c18d67febdc3ede60a66457
Austin Jeremiah J <139321386+austinjeremiah@users.noreply.github.com>
2024-12-17 19:08:27 +0530

    Update lib.rs

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -9,10 +9,48 @@ declare_id!("4MFbPXXDsa62XnCaHdAijgScgPr2qaKi7cX6DZg1eTUb");
 pub mod votingdapp {
     use super::*;
 
-    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()> {
+    pub fn initialize_poll(ctx: Context<InitializePoll>,
+                            poll_id: u64,
+                            description: String,
+                            poll_start: u64,
+                            poll_end: u64) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.poll_start= poll_start;
+        poll.poll_end= poll_end;
+        poll.candidate_amount=0;
         Ok(())
     }
+
+    pub fn initialize_canditate(ctx: Context<InitializeCandidate>,
+                                candidate_name: String,
+                                poll_id: 64) -> Result <()>{
+
+        Ok(())
+
+                                    
+
+    }
 }
+#[derive(Accounts)]
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info>{
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Poll::INIT_SPACE, 
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info, Poll>,
+    pub system_program: Program<'info, System>,
+     
+}
+
 #[derive(Accounts)]
 #[instruction(poll_id: u64)]
 pub struct InitializePoll<'info> {
