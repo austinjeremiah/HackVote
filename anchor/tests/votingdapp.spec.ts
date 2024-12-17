7df85724657e259bc8167eb76b75070e813e0d7c
Austin Jeremiah J <139321386+austinjeremiah@users.noreply.github.com>
2024-12-17 22:27:12 +0530

    Update votingdapp.spec.ts by adding functions and testing output

--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -6,26 +6,90 @@ import { BankrunProvider, startAnchor } from "anchor-bankrun";
 
 const IDL = require('../target/idl/votingdapp.json');
 
-const votingdappAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+const votingdappAddress = new PublicKey("4MFbPXXDsa62XnCaHdAijgScgPr2qaKi7cX6DZg1eTUb");
 
 describe('votingdapp', () => {
 
-  it('Initialize Poll', async () => {
-    const context = await startAnchor("", [{ name: "votingdapp", programId: votingdappAddress}], []);
-	  const provider = new BankrunProvider(context);
+  let context;
+  let provider;
+  let votingdappProgram: Program<Votingdapp>;
+
+
+
+  beforeAll (async () => {
+
+    context = await startAnchor("", [{ name: "votingdapp", programId: votingdappAddress}], []);
+	  provider = new BankrunProvider(context);
 
     const votingdappProgram = new Program<Votingdapp>(
       IDL,
       provider,
     );
 
+    });
+
+  it('Initialize Poll', async () => {
+    jest.setTimeout(30000);
+    context = await startAnchor("", [{ name: "votingdapp", programId: votingdappAddress}], []);
+	  provider = new BankrunProvider(context);
+
+    votingdappProgram = new Program<Votingdapp>(
+      IDL,
+      provider,
+    );
+
     await votingdappProgram.methods.initializePoll(
         new anchor.BN(1),
-        "Whats you favorite web3 ecosystem ?",
-        new anchor.BN(1834365552),
+        "What is your favourite web3 ecosystem ?",
         new anchor.BN(0),
-       ).rpc();
+        new anchor.BN(1844365552),
+        ).rpc();
+
+        const [pollAddress] = PublicKey.findProgramAddressSync(
+            [new anchor.BN(1).toArrayLike(Buffer,'le', 8)],
+            votingdappAddress,
+        )
+        const Poll = await votingdappProgram.account.poll.fetch(pollAddress);
+        console.log(Poll);
+        expect(Poll.pollId.toNumber()).toEqual(1);
+        expect(Poll.description).toEqual("What is your favourite web3 ecosystem ?");
+        expect(Poll.pollStart.toNumber()).toBeLessThan(Poll.pollEnd.toNumber());
+
   });
+
+  it("initialize_candidate", async () =>{
+    await votingdappProgram.methods.initializeCanditate(
+      "Solana",
+      new anchor.BN(1)
+  ).rpc();
+  
+  await votingdappProgram.methods.initializeCanditate(
+      "Ethereum",
+      new anchor.BN(1)
+  ).rpc();
+
+  const [solanaAddress] = PublicKey.findProgramAddressSync(
+    [new anchor.BN(1).toArrayLike(Buffer,'le', 8), Buffer.from("Solana")],
+     votingdappAddress,
+  );
+   const solanaCandidate = await votingdappProgram.account.canditate.fetch(solanaAddress);
+   console.log(solanaCandidate);
+
+   const [ethereumAddress] = PublicKey.findProgramAddressSync(
+    [new anchor.BN(1).toArrayLike(Buffer,'le', 8), Buffer.from("Solana")],
+     votingdappAddress,
+  );
+   const ethereumCandidate = await votingdappProgram.account.canditate.fetch(ethereumAddress);
+   console.log(ethereumCandidate);
+
+  });
+
+  it("vote", async () =>{
+
+
+    
+  });
+
 
 });
  
